7e936101298add59edd1e18339d5fe0397b48e20
Lye2002 <2072981423@qq.com>
2022-07-01 02:16:55 +0800

    123

--- Shop_client/src/main/java/Bean/UserLogin.java
+++
@@ -1,9 +0,0 @@
-package Bean;
-
-import lombok.Data;
-
-@Data
-public class UserLogin {
-    private String username;
-    private String password;
-}
--- Shop_client/src/main/java/config/Config.java
+++
@@ -1,5 +0,0 @@
-package config;
-
-public  class  Config {
-    public static String BASE_URL="http://localhost:8081";
-}
